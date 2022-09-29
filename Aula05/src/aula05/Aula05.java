a181747ca376fc4d84630afbd7bb7cc49b1204aa
Fernanda <Fernanda@DESKTOP-TJMPV3V>
2022-09-29 13:33:39 -0300

    criei um arquivo java

--- Aula05/src/aula05/Aula05.java
+++ Aula05/src/aula05/Aula05.java
@@ -4,19 +4,18 @@
  */
 package aula05;
 
-
 public class Aula05 {
 
     public static void main(String[] args) {
-       ContaNoBanco c1  = new ContaNoBanco(false, 0f);
-       c1.abrirConta("Cc");
-       c1.setTipo("Corrente");
-       c1.setSaldo(10f);
-       c1.setnumConta(1459784260);
-       c1.setDono("Fernanda Coelho");
-       c1.estadoConta();
-       c1.depositar(5f);
-       
+        ContaNoBanco c1 = new ContaNoBanco(false, 0f);
+        c1.abrirConta("Cc");
+        c1.setTipo("Corrente");
+        c1.setSaldo(10f);
+        c1.setnumConta(1459784260);
+        c1.setDono("Fernanda Coelho");
+        c1.estadoConta();
+        c1.depositar(5f);
+
     }
-    
+
 }
--- Aula05/src/aula05/ContaNoBanco.java
+++ Aula05/src/aula05/ContaNoBanco.java
@@ -5,6 +5,7 @@
 package aula05;
 
 public class ContaNoBanco {
+
     public int numConta;
     protected String tipo;
     private String dono;
@@ -14,7 +15,7 @@ public class ContaNoBanco {
     public ContaNoBanco(boolean status, float saldo) {
         this.status = status;
         this.saldo = saldo;
-    } 
+    }
 
     public int getnumConta() {
         return numConta;
@@ -47,86 +48,87 @@ public class ContaNoBanco {
     public void setSaldo(float saldo) {
         this.saldo = saldo;
     }
-    
-    public boolean getStatus(){
+
+    public boolean getStatus() {
         return this.status;
     }
-    public void setStatus(boolean s){
+
+    public void setStatus(boolean s) {
         this.status = s;
     }
 
-    public void abrirConta(String t){
+    public void abrirConta(String t) {
         setTipo(t);
         setStatus(true);
-        if (t == "Cc"){
+        if (t == "Cc") {
             this.saldo = 50;
-        }else if(t == "Cp"){
-            this.saldo+= 150;
-         
+        } else if (t == "Cp") {
+            this.saldo += 150;
+
         }
-   
+
     }
-    public void fecharConta(){
-        if (saldo > 0){
+
+    public void fecharConta() {
+        if (saldo > 0) {
             System.out.println("Conta com dinheiro!");
-        }else if (saldo < 0){
+        } else if (saldo < 0) {
             System.out.println("Conta em débito");
-        }else{
+        } else {
             setStatus(false);
         }
-        
+
     }
-        
-    public void depositar(float deposito){
-        if (this.getStatus() == true){
+
+    public void depositar(float deposito) {
+        if (this.getStatus() == true) {
             setSaldo(getSaldo() + deposito);
             //setSaldo(getSaldo() + deposito);
-            System.out.println("Valor do depósito: "+ deposito);
-            System.out.println("Saldo Atual: "+ getSaldo());
-        }else{
+            System.out.println("Valor do depósito: " + deposito);
+            System.out.println("Saldo Atual: " + getSaldo());
+        } else {
             System.out.println("Impossível Depositar");
         }
     }
-    public void sacar(float saque){
-        if (getStatus() == true){
-            
-            if (getSaldo() >= saque){
+
+    public void sacar(float saque) {
+        if (getStatus() == true) {
+
+            if (getSaldo() >= saque) {
                 setSaldo(getSaldo() - saque);
-                System.out.println("Valor do saque: "+ saque);
-                System.out.println("Saldo Atual: "+ this.getSaldo());
-            }else{
+                System.out.println("Valor do saque: " + saque);
+                System.out.println("Saldo Atual: " + this.getSaldo());
+            } else {
                 System.out.println("Saldo insuficiente para realizar o saque!");
             }
-        }else{
+        } else {
             System.out.println("Conta não existe. Impossível sacar!");
         }
-        
-    }
-    public void pagarMensal(){
-        float v;
-        if (getTipo() == "Cc"){
-            v = 12;
-        }else if (getTipo() == "Cp"){
-            v = 20;
-         if (getStatus() == true){
-            if (getSaldo() > v){
-                setSaldo(getSaldo() - v);
-                
-            }
-        }
-        
-     }
-        
-    }
-    
-    public void estadoConta(){
-        System.out.println("Dados sobre a Conta "+ this.getTipo());
-        System.out.println("Número: "+ this.getnumConta());
-        System.out.println("Tipo: "+ this.getTipo());
-        System.out.println("Dono: "+ this.getDono());
-        System.out.println("Saldo: "+ this.getSaldo())
-        
-         ;
-    }
-   }
 
+    }
+
+    public void pagarMensal() {
+        float v;
+        if (getTipo() == "Cc") {
+            v = 12;
+        } else if (getTipo() == "Cp") {
+            v = 20;
+            if (getStatus() == true) {
+                if (getSaldo() > v) {
+                    setSaldo(getSaldo() - v);
+
+                }
+            }
+
+        }
+
+    }
+
+    public void estadoConta() {
+        System.out.println("Dados sobre a Conta " + this.getTipo());
+        System.out.println("Número: " + this.getnumConta());
+        System.out.println("Tipo: " + this.getTipo());
+        System.out.println("Dono: " + this.getDono());
+        System.out.println("Saldo: " + this.getSaldo());
+    }
+}
